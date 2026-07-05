29483f5e542b0b18d5167a0fd94465eeb3a360c3
thomasjacob04 <thomasjacob04@gmail.com>
2026-07-05 22:21:52 +1000

    no dark or light mode, just css

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,11 +14,6 @@ export const metadata: Metadata = {
     icon: [
       {
         url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
       },
       {
         url: '/icon.svg',
